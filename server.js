b4a2c59c335d7251d677ee8dc7c630f0f90701da
CristinaS-Tehno <71755060+CristinaS-Tehno@users.noreply.github.com>
2020-09-23 10:48:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const WebSocket = require('ws');
 const path = require('path');
 const EventHubReader = require('./scripts/event-hub-reader.js');
 
-const iotHubConnectionString = process.env.HostName=Tehno.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=npycigPkkL67x4pyQEU6y8DFSuQVW3GP/kR4EDkp26s=;
+const iotHubConnectionString = process.env.IotHubConnectionString;
 if (!iotHubConnectionString) {
   console.error(`Environment variable IotHubConnectionString must be specified.`);
   return;
 }
 console.log(`Using IoT Hub connection string [${iotHubConnectionString}]`);
 
-const eventHubConsumerGroup = process.env.Tehno;
+const eventHubConsumerGroup = process.env.eventHubConsumerGroup;
 set EventHubConsumerGroup=Tehno;
 console.log(eventHubConsumerGroup);
 if (!eventHubConsumerGroup) {
